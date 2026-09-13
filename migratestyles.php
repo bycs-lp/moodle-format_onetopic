8bc477c32685aa304ce7b42da5bc3bccaec63071
Peter Mayer <admin@pemasoft.de>
2026-09-14 00:09:53 +0200

    MBS-11071: Escape untrusted output to prevent XSS

--- migratestyles.php
+++ migratestyles.php
@@ -182,7 +182,7 @@ if (empty($customstyles)) {
 
         $cssstyles = [];
         foreach ($section->styles as $style) {
-            $cssstyles[] = $style->name . ': ' . $style->value;
+            $cssstyles[] = s($style->name . ': ' . $style->value);
         }
 
         $newdefined = isset($stylesnew[$sectionid]);
@@ -201,7 +201,7 @@ if (empty($customstyles)) {
         }
 
         $table->data[] = [
-            $section->coursename,
+            s($section->coursename),
             $sectionlink,
             implode(', ', $cssstyles),
             $newdefined ? get_string('no') : get_string('yes'),
